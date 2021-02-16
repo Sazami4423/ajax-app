fdc9d5bfd5ce72498aec2d064de237b4ccea6bd8
Sazami4423 <78459786+Sazami4423@users.noreply.github.com>
2021-02-16 22:52:02 +0900

    firstcommit

---
+++ app/models/message.rb
@@ -0,0 +1,13 @@
+class Message < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
+  has_one_attached :image
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+  # バリテーションの設定
+  # 異常なし
+end
